de38087eac2fed4b5bc4cb6ee11f4d964b189409
metal-crow <frankcrisanti@aol.com>
2017-11-09 22:37:10 -0500

    change printout for z3 solution

--- src/solveZ3HornClauses.scala
+++ src/solveZ3HornClauses.scala
@@ -11,11 +11,19 @@ object solveZ3HornClauses {
     for(line <- clauses){
       writer.write(line+"\n");
     }
-    writer.write("(check-sat)\n");
-    writer.write("(get-model)\n");
+    writer.write("(check-sat)");
     writer.close();
     
-    "z3 -smt2 Z3HornClauses.txt"!
+    val result = "z3 -smt2 Z3HornClauses.txt"!!;
+    if(result.stripLineEnd.equals("sat")){
+      println("correct");
+    }
+    else if(result.stripLineEnd.equals("unsat")){
+      println("incorrect");
+    }
+    else{
+      println(result);
+    }
   }
   
 }
